236d9ab2a10958d42e36f4f4af31ac93186dc996
Heeseok-Jeong <dldydldy@naver.com>
2018-04-07 02:20:52 +0900

    I don't know why it does not working

--- src/main/java/edu/handong/csee/java/Lab09/SalesAnalyzer.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesAnalyzer.java
@@ -14,92 +14,94 @@ public class SalesAnalyzer { //the class name is SalesAnalyzer
 	private SalesAssociate[] team; //declare an array which type is SalesAssociate
 	
 	/**
-	 * 
+	 * gets information of salesmen and shows the result comparing with average
 	 */
 	public static void main(String[] args) { //main method
 		int i; //declare a variable i
 		Scanner keyboard = new Scanner(System.in); //instantiate a keyboard
 		SalesAnalyzer sAnalyze = new SalesAnalyzer(); //instantiate sAnalyze
 		
-		
 		System.out.println("Enter number of sales associates : "); //print some message
 		int times = keyboard.nextInt(); //times gets value from user
-		SalesAssociate[] team = new SalesAssociate[times]; //instantiate(?) SaluesAssociate type array team 
+		System.out.println("");
+		//SalesAssociate[] mTeam = new SalesAssociate[times]; //instantiate(?) SalesAssociate type array team 
 		
 		for(i=0; i < times; i++) { //repeat as times
-			System.out.println("Enter data for associate number " + i+1); //print some message
+			System.out.println("Enter data for associate number " + (i+1)); //print some message
 			sAnalyze.getData(i); //gets person(i)'s value
 			System.out.println(""); //make a one line
 		}
 		
 		sAnalyze.computeAverage(); //computes sales average
-		sAnalyze.computeHighestSales();
+		sAnalyze.computeHighestSales(); //computes that highest person's name, sales and index
 		
-		sAnalyze.displayResults();
+		sAnalyze.displayResults(); //print all the results
 	}
 
 	/**
 	 * gets a data of name and sales of person
 	 */
 	public void getData(int i) { //method name is getData
+		//SalesAssociate[] mTeam = new SalesAssociate[i]; //instantiate(?) SalesAssociate type array team 
 		Scanner keyboard = new Scanner(System.in); //instantiate keyboard to use scanner
 		System.out.print("Enter name of sales associate : "); //print some message
-		team[i].name = keyboard.nextLine();  //name gets string type value from user 
+		this.team[i].name = keyboard.nextLine();  //name gets string type value from user 
 		System.out.print("Enter associate's sales : $"); //print some message
-		team[i].sales = keyboard.nextDouble(); //sales gets double type value from user
+		this.team[i].sales = keyboard.nextDouble(); //sales gets double type value from user
+		
 	}
 		
 		
 	
 	/**
-	 * 
+	 * this method computes a sales average
 	 */
-	public void computeAverage() {
+	public void computeAverage() { //this method name is computeAverage
 		double sum = 0;
-		for(int i=0; i < team.length; i++) {
-			sum += team[i].sales;
+		for(int i=0; i < this.team.length; i++) {
+			sum += this.team[i].sales;
 		}
 		this.averageSales = sum / team.length;
 	}
 	
 	/**
-	 * 
+	 * this method computes informations of who sells the highest prices
 	 */
-	public void computeHighestSales() {
-		this.highestSales = 0;
-		for(int i=0; i < team.length; i++) 
-			if(highestSales <= team[i].sales) {
-				highestSales = team[i].sales;
-				this.highestName = team[i].name;
-				this.highestNum = i;
+	public void computeHighestSales() { //this method name is computeHighestSales
+		this.highestSales = 0; //this variable's value is 0
+		for(int i=0; i < this.team.length; i++)  //loop until i's value is same team.length
+			if(highestSales <= this.team[i].sales) { //if highestSales value is smaller or equal team[i].sales value
+				highestSales = this.team[i].sales; //highestSalesvalue is that value
+				this.highestName = this.team[i].name; //highestName is that name
+				this.highestNum = i; //highestNum is that index
 			}
 	}
 	
 	/**
-	 * 
+	 * this method shows sales average and highest sales ,also each people's evaluation comparing with average
 	 */
-	public void displayResults() {
-		System.out.println("Average sales per associate is $" + this.averageSales);
-		System.out.println("The highest sales figure is $" + this.highestSales);
-		System.out.println("");
+	public void displayResults() { //this method name is displayResults
+		System.out.println("Average sales per associate is $" + this.averageSales); //print sales average
+		System.out.println("The highest sales figure is $" + this.highestSales); //print highest sales
+		System.out.println(""); //add one line
 		
-		for(int i=0; i < team.length; i++) {
-			if(i == this.highestNum) {
-				System.out.println("The following had the highest sales : ");
-				System.out.println("Name : " + this.highestName);
-				System.out.println("Sales : " + this.highestSales);
+		for(int i=0; i < this.team.length; i++) { //loop until i's value is same team.length
+			if(i == this.highestNum) {  //if i is same this.highestNum
+				System.out.println("The following had the highest sales : "); //print some message
+				System.out.println("Name : " + this.highestName); //print highest seller's name
+				System.out.println("Sales : " + this.highestSales); //print highest sales
 				System.out.println("$" + (this.highestSales-this.averageSales) + "above the average.");
 			}
 			
 			System.out.println("The rest performed as follows :");
-			System.out.println("Name : " + team[i].name);
-			System.out.println("Sales : $" + team[i].sales);
-			if(team[i].sales > this.averageSales) 
-				System.out.println("$" + team[i].sales + " aobve the average.");
-			if(team[i].sales == this.averageSales)
-				System.out.println("$" + team[i].sales + " same the average.");
-			if(team[i].sales < this.averageSales)
-				System.out.println("$" + team[i].sales + " below the average.");
+			System.out.println("Name : " + this.team[i].name);
+			System.out.println("Sales : $" + this.team[i].sales);
+			if(this.team[i].sales > this.averageSales) 
+				System.out.println("$" + (this.team[i].sales-this.averageSales) + " aobve the average.");
+			if(this.team[i].sales == this.averageSales)
+				System.out.println("$" + (this.team[i].sales-this.averageSales) + " same the average.");
+			if(this.team[i].sales < this.averageSales)
+				System.out.println("$" + (this.averageSales - this.team[i].sales) + " below the average.");
 	
 		}
 	}
--- src/main/java/edu/handong/csee/java/Lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesAssociate.java
@@ -1,39 +1,40 @@
 package edu.handong.csee.java.Lab09;
 
 /**
- * 
+ * This class is a data of SalesAssociate which contains name and sales.
+ * Furthermore, it has setter and getter of each members.
  * @author JHS
  */
-public class SalesAssociate {
-	String name;
-	double sales;
+public class SalesAssociate { //class name is SalesAssociate
+	String name; //first member is string type name
+	double sales; //second member is double type sales
 	
 	/**
-	 * 
+	 * getter of name
 	 */
-	public String getName() {
-		return name;
+	public String getName() { //this method name is getName
+		return name; //return a string of name
 	}
 	
 	/**
-	 * 
+	 * setter of name
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String name) { //this method name is setName
+		this.name = name; //sets the instance variable name with parameter
 	}
 	
 	/**
-	 * 
+	 * getter of sales
 	 */
-	public double getSales() {
-		return sales;
+	public double getSales() { //this method name is getSales
+		return sales; //return a value of name
 	}
 	
 	/**
-	 * 
+	 * setter of sales
 	 */
-	public void setSales(double sales) {
-		this.sales = sales;
+	public void setSales(double sales) { //this method name is setSales
+		this.sales = sales; //sets the instance variable sales with parameter
 	}
 	
 }
