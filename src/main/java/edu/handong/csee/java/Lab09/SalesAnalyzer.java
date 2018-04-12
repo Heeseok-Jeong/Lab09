ac7f0c05de24738fc5e7552f89327c6d2fadfe14
Heeseok-Jeong <dldydldy@naver.com>
2018-04-12 18:56:05 +0900

    Write all comments

--- src/main/java/edu/handong/csee/java/Lab09/SalesAnalyzer.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesAnalyzer.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner; //call a package to use scanner
 
 /**
- * This program gets number of salesmen, their names, sales and computes average of sales. 
+ * This program gets number of salesmen, their names, sales and computes average of sales by using ArrayList. 
  * Also, shows who sells the highest profit comparing with average and shows other members results. 
  * @author JHS
  */
@@ -11,45 +11,45 @@ public class SalesAnalyzer { //the class name is SalesAnalyzer
 	private double averageSales; //declare an instance variable
 	private double highestSales; //declare an instance variable
 	private String highestName; //declare an instance variable 
-	private SalesAssociate sAssociate = new SalesAssociate();
-	private String name = sAssociate.name;
-	private Double sales = sAssociate.sales;
-	private ArrayList<String> teamName = new ArrayList<String>();
-	private ArrayList<Double> teamSales = new ArrayList<Double>();
-	private int listSize;
-	Scanner keyboard = new Scanner(System.in); //instantiate a keyboard
+	private SalesAssociate sAssociate = new SalesAssociate(); //instantiate SalesAssociate class
+	private String name = sAssociate.name; //instantiate name in the SalesAssociate class
+	private Double sales = sAssociate.sales; //instantiate sales in the SalesAssociate class
+	private ArrayList<String> teamName = new ArrayList<String>(); //instantiate String type ArrayList 'teamName'
+	private ArrayList<Double> teamSales = new ArrayList<Double>(); //instantiate Double type ArrayList 'teamSales'
+	private int listSize; //declare an instance variable 
+	Scanner keyboard = new Scanner(System.in); //instantiate Scanner
 	
 	/**
-	 * gets information of salesmen, computes stats and shows the result comparing with average.
+	 * gets information of salesmen, computes several stats and shows the result comparing with an average.
 	 */
 	public static void main(String[] args) { //main method
 		SalesAnalyzer sAnalyze = new SalesAnalyzer(); //instantiate this class
-		sAnalyze.getData(); //get some people's data
+		sAnalyze.getData(); //gets some people's data
 		sAnalyze.computeStats(); //computes sales average and that highest person's name and sales
-		sAnalyze.displayResults(); //print all the results
+		sAnalyze.displayResults(); //prints all the results
 	}
 
 	/**
-	 * gets a name and sales of person
+	 * gets a name and sales of person. The way how many people we take it is the word 'yes' which get from user
 	 */
 	public void getData() { //method name is getData
-		int i = 1;
-		boolean done = true;
+		int i = 1; //initialize a local variable
+		boolean done = true; //initialize a local variable for loop
 		
-		while(done) { //repeat as times
+		while(done) { //if boolean type done is true, below things will loops
 			System.out.println("Enter data for associate number " + i++); //print some message
 			System.out.print("Enter name of sales associate : "); //print some message
-			String name = keyboard.nextLine();  //name gets string type value from user 
-			teamName.add(name);
+			String name = keyboard.nextLine();  //local variable name gets string type value from user 
+			teamName.add(name); //local variable name's value is added into the ArrayList teamName
 			System.out.print("Enter associate's sales : $"); //print some message
-			Double sales = keyboard.nextDouble(); //sales gets double type value from user
-			teamSales.add(sales);
+			Double sales = keyboard.nextDouble(); //local variable sales gets double type value from user
+			teamSales.add(sales); //local variable sales's value is added into the ArrayList teamSales
 			keyboard.nextLine(); //this is for skipping enter key
 			System.out.println(""); //print one line
-			System.out.print ("More items for the list?('yes' for run or other key) ");
-			String ans = keyboard.nextLine ();
-            if (!ans.equalsIgnoreCase ("yes"))
-                done = false;
+			System.out.print ("More items for the list?('yes' for run or other key) "); //ask for user input 'yes' or not  
+			String ans = keyboard.nextLine (); //local variable ans gets the answer from user
+            if (!ans.equalsIgnoreCase ("yes")) //if ans's value isn't 'yes' or 'YES', 
+                done = false; //done's value is false, then this loop will stop
             System.out.println(""); //print one line
 		}
 	}	
@@ -57,17 +57,14 @@ public class SalesAnalyzer { //the class name is SalesAnalyzer
 	/**
 	 * this method computes a sales average and informations of who sells the highest prices
 	 */
-	public void computeStats() { //this method name is computeAverage
-		listSize = teamSales.size(); 
-		highestSales = 0.0;
-		double sum = 0.0; //declare a local variable
+	public void computeStats() { //this method name is computeStats
+		listSize = teamSales.size(); //the instance variable listSize's values is ArrayList teamSales's size(length)
+		highestSales = 0.0; //set the instance variable highestSales's value zero
+		double sum = 0.0; //initialize local variable sum's value zero
 		for(int i = 0; i < listSize; i++) { //repeat listSize times
-			sum += teamSales.get(i); //keep summing person's sales value into sum
+			sum += teamSales.get(i); //keep summing each people's sales value into sum
 			
-			
-			//System.out.println(highestSales);
-			//System.out.println(highestName);
-			if(highestSales < teamSales.get(i)) { //if highestSales value is less than team[i].sales value
+			if(highestSales <= teamSales.get(i)) { //if highestSales value is less than teamSales[i] value
 				highestSales = teamSales.get(i); //highestSalesvalue is that value
 				highestName = teamName.get(i); //highestName is that name
 			}
@@ -77,7 +74,7 @@ public class SalesAnalyzer { //the class name is SalesAnalyzer
 	}
 	
 	/**
-	 * this method shows sales average and highest sales ,also each people's evaluation comparing with average
+	 * this method shows sales average and highest sales, also each people's evaluation comparing with average
 	 */
 	public void displayResults() { //this method name is displayResults
 		System.out.println("Average sales per associate is $" + averageSales); //print sales average
@@ -85,21 +82,21 @@ public class SalesAnalyzer { //the class name is SalesAnalyzer
 		System.out.println(""); //add one line
 		
 		System.out.println("The following had the highest sales : "); //print some message
-		System.out.println("Name : " + highestName); //print highest seller's name
-		System.out.println("Sales : " + highestSales); //print highest sales
-		System.out.println("$" + (highestSales-averageSales) + "above the average.");
+		System.out.println("Name : " + highestName); //print the highest seller's name
+		System.out.println("Sales : " + highestSales); //print the highest sales
+		System.out.println("$" + (highestSales-averageSales) + "above the average."); //print how much different between sales and average
 		System.out.println(""); //add one line
 		System.out.println("The rest performed as follows :"); //print some message
 		
-		for(int i=0; i < listSize; i++) { //loop until i's value is same team.length
-			if(teamSales.get(i) != highestSales) {  //if i is same this.highestNum
-				System.out.println("Name : " + teamName.get(i)); //print team[i]'s name
-				System.out.println("Sales : $" + teamSales.get(i)); //print team[i]'s sales
-				if(teamSales.get(i) > averageSales)  //and if team[i]'s sales is larger than average
+		for(int i=0; i < listSize; i++) { //repeat listSize times
+			if(teamSales.get(i) != highestSales) {  //if teamSales[i]'s value isn't same highestSales 
+				System.out.println("Name : " + teamName.get(i)); //print teamName[i]'s value(name)
+				System.out.println("Sales : $" + teamSales.get(i)); //print teamSales[i]'s value(sales)
+				if(teamSales.get(i) > averageSales)  //and if teamSales[i]'s value is larger than average
 					System.out.println("$" + (teamSales.get(i) - averageSales) + " above the average."); //print how much different between sales and average
-				if(teamSales.get(i) == averageSales)  //or if team[i]'s sales is same than average
+				else if(teamSales.get(i) == averageSales)  //or if teamSales[i]'s sales is same average
 					System.out.println("$" + (teamSales.get(i) - averageSales) + " same the average."); //print how much different between sales and average
-				if(teamSales.get(i) < averageSales) //or if team[i]'s sales is smaller than average
+				else if(teamSales.get(i) < averageSales) //or if teamSales[i]'s sales is smaller than average
 					System.out.println("$" + (averageSales - teamSales.get(i)) + " below the average."); //print how much different between sales and average
 			}
 		}
